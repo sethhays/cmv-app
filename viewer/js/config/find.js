7352cafb3432575c8dcfcfaa21ccbc9b63d65bcd
Jade Freeman <freema23@msu.edu>
2014-08-24 15:00:30 -0400

    Configure to support separate applications via url config parameter.  Import FacilityInfrastructure configurations.

--- viewer/js/config/find.js
+++ viewer/js/config/find.js
@@ -1,12 +1,26 @@
 define({
     map: true,
-    queries: [
-        {
-            description: 'Buildings',
-            url: 'https://fis.ipf.msu.edu/arcgis/rest/services/Locators/PublicFeatures/MapServer',
-            layerIds: [0],
-            searchFields: ['BUILDING','NAME','LOCATION'],
-            minChars: 2
-        }
-    ]
+           queries: [
+               {
+                   description: 'Buildings',
+                   url: 'https://fis.ipf.msu.edu/arcgis/rest/services/Locators/PublicFeatures/MapServer',
+                   layerIds: [0],
+                   searchFields: ['BUILDING','NAME','LOCATION'],
+                   minChars: 2
+               },
+               {
+                   description: 'Parking Lots',
+                   url: 'https://fis.ipf.msu.edu/arcgis/rest/services/Locators/PublicFeatures/MapServer',
+                   layerIds: [1],
+                   searchFields: ['BUILDING','NAME','LOTNUM','DESCRIPTION'],
+                   minChars: 2
+               },
+               {
+                   description: 'Buildings',
+                   url: 'https://fis.ipf.msu.edu/arcgis/rest/services/Locators/PublicFeatures/MapServer',
+                   layerIds: [2],
+                   searchFields: ['BUILDING','NAME','DESCRIPTION'],
+                   minChars: 2
+               }
+           ]
 });
