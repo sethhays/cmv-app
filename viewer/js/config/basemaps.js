b6f1644a33748fbd78a112763a0d17d067e4f25a
Jade Freeman <freema23@msu.edu>
2014-09-14 16:00:33 -0400

    Add additional basemaps

--- viewer/js/config/basemaps.js
+++ viewer/js/config/basemaps.js
@@ -12,17 +12,59 @@ define([
         mapStartBasemap: 'msuCampus',
         //basemaps to show in menu. define in basemaps object below and reference by name here
         // TODO Is this array necessary when the same keys are explicitly included/excluded below?
-        basemapsToShow: ['msuCampus','msuAerial'],
+        basemapsToShow: ['msuCampus','msuMapsCampus', 'engineering', 'msu2012Aerial', 'msu2010Aerial'],
 
         // define all valid custom basemaps here. Object of Basemap objects. For custom basemaps, the key name and basemap id must match.
         basemaps: {
-            msuAerial   : {
+            msu2010Aerial   : {
                 title  : '2010 Aerial',
                 basemap: new Basemap ( {
-                        id    : 'msuAerial',
+                        id    : 'msu2010Aerial',
                         layers: [
                             new BasemapLayer ( { url: 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer' } ),
                             new BasemapLayer ( { url: 'http://prod.gis.msu.edu/arcgis/rest/services/basemap/aerial/MapServer' } )
+                        ]
+                    }
+                )
+            },
+            msu2012Aerial   : {
+                title  : '2012 Aerial',
+                basemap: new Basemap ( {
+                                           id    : 'msu2012Aerial',
+                                           layers: [
+                                               new BasemapLayer ( { url: 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer' } ),
+                                               new BasemapLayer ( { url: 'http://prod.gis.msu.edu/arcgis/rest/services/historical/msu2012_jun19/MapServer' } )
+                                           ]
+                                       }
+                )
+            },
+            msuA2011erial   : {
+                title  : '2011 Aerial',
+                basemap: new Basemap ( {
+                                           id    : 'msu2011Aerial',
+                                           layers: [
+                                               new BasemapLayer ( { url: 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer' } ),
+                                               new BasemapLayer ( { url: 'http://prod.gis.msu.edu/arcgis/rest/services/historical/msu2011_nov15/MapServer' } )
+                                           ]
+                                       }
+                )
+            },
+            worldImagery   : {
+                title  : 'World Imagery',
+                basemap: new Basemap ( {
+                                           id    : 'worldImagery',
+                                           layers: [
+                                               new BasemapLayer ( { url: 'http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer' } )
+                                           ]
+                                       }
+                )
+            },
+            engineering   : {
+                title  : 'Engineering Style',
+                basemap: new Basemap ( {
+                        id    : 'engineering',
+                        layers: [
+                            new BasemapLayer ( { url: 'https://fis.ipf.msu.edu/arcgis/rest/services/Basemaps/EngineeringBasemap/MapServer' } )
                         ]
                     }
                 )
@@ -39,7 +81,18 @@ define([
                         ]
                     }
                 )
+            },
+            msuMapsCampus   : {
+                title  : 'maps.msu.edu',
+                basemap: new Basemap ( {
+                        id    : 'msuMapsCampus',
+                        layers: [ new BasemapLayer ( { url: 'http://services.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer' } ),
+                            new BasemapLayer ( { url: 'http://prod.gis.msu.edu/arcgis/rest/services/msu/basemap/MapServer' } )
+                        ]
+                    }
+                )
             }
         }
     };
 });
+
